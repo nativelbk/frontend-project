7154aeb84d33929c914254514a5cccc94b75fd86
Nativel <nativelbk@gmail.com>
2024-02-11 09:52:37 +0100

    animation improved

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -23,7 +23,7 @@ export default function Conatcts() {
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           whileInView={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 0.8 }}
           viewport={{ once: true }}
           className="w-[300px]"
         >
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -3,7 +3,13 @@ import { motion } from "framer-motion";
 
 export default function Project() {
   return (
-    <motion.div className="h-[400px] max-[388px]:w-[300px] max-[1008px]:w-[280px] max-[643px]:w-[330px]   max-[1144px]:w-[300px]  w-[330px] flex justify-center  py-4 bg-cardBackground rounded-3xl ">
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+      className="h-[400px] max-[388px]:w-[300px] max-[1008px]:w-[280px] max-[643px]:w-[330px]   max-[1144px]:w-[300px]  w-[330px] flex justify-center  py-4 bg-cardBackground rounded-3xl "
+    >
       <div className=" w-[300px] max-[388px]:w-[280px] max-[643px]:w-[300px] max-[1008px]:w-[260px] max-[1144px]:w-[270px] flex flex-col gap-4 ">
         <img src="/mademoiselleai.svg" />
         <div className="text-white flex flex-col gap-5 w-[95%] mx-auto ">

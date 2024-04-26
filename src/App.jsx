b2ae2612036618225362d979e202daedf9f4b85f
nativelbk <129794127+nativelbk@users.noreply.github.com>
2024-04-26 13:24:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,58 +19,58 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import { motion } from "framer-motion";
 import Particles from "./components/Hero/Particles";
-function App() {
-  return (
-    <main className="flex overflow-x-hidden  max-md:gap-5 relative flex-col ">
-      <Navbar />
-      {/* <img
-        src="/Achernar.svg"
-        className=" z-[-1]  absolute right-[-20%]  top-[-60px]   "
-      /> */}
-      <div className="absolute z-[-1] top-16  right-[0]  ">
-        <motion.div
-          animate={{
-            background: ["#1976D2", "#FF4D6D"],
-            scale: [1, 0.95, 1.1],
-          }}
-          transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
-          className=" blr max-sm:hidden"
-        ></motion.div>
-        <motion.div
-          animate={{
-            background: ["#1976D2", "#152432"],
-            scale: [1, 0.95, 1.1],
-          }}
-          transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
-          className=" blr max-sm:hidden "
-        ></motion.div>
-        <motion.div
-          animate={{
-            background: ["#1976D2", "#7B2CBF"],
-            scale: [1, 0.95, 1.1],
-          }}
-          transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
-          className=" blr max-sm:hidden  "
-        ></motion.div>
-      </div>
-      <div className="flex flex-col max-md:gap-40">
-        <Hero />
-        <About />
-        <Experience />
-      </div>
-      <div className="flex overflow-x-hidden justify-between  max-sm:gap-96 relative flex-col ">
-        <Skills />
-        <Projects />
-        <Conatcts />
-      </div>
-      <img
-        src="/Achernar.svg"
-        className=" z-[-1] absolute right-[-20%]  top-[-60px]   "
-      />
-      <Particles/>
-      <Footer />
-    </main>
-  );
-}
+// function App() {
+//   return (
+//     <main className="flex overflow-x-hidden  max-md:gap-5 relative flex-col ">
+//       <Navbar />
+//       {/* <img
+//         src="/Achernar.svg"
+//         className=" z-[-1]  absolute right-[-20%]  top-[-60px]   "
+//       /> */}
+//       <div className="absolute z-[-1] top-16  right-[0]  ">
+//         <motion.div
+//           animate={{
+//             background: ["#1976D2", "#FF4D6D"],
+//             scale: [1, 0.95, 1.1],
+//           }}
+//           transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
+//           className=" blr max-sm:hidden"
+//         ></motion.div>
+//         <motion.div
+//           animate={{
+//             background: ["#1976D2", "#152432"],
+//             scale: [1, 0.95, 1.1],
+//           }}
+//           transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
+//           className=" blr max-sm:hidden "
+//         ></motion.div>
+//         <motion.div
+//           animate={{
+//             background: ["#1976D2", "#7B2CBF"],
+//             scale: [1, 0.95, 1.1],
+//           }}
+//           transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
+//           className=" blr max-sm:hidden  "
+//         ></motion.div>
+//       </div>
+//       <div className="flex flex-col max-md:gap-40">
+//         <Hero />
+//         <About />
+//         <Experience />
+//       </div>
+//       <div className="flex overflow-x-hidden justify-between  max-sm:gap-96 relative flex-col ">
+//         <Skills />
+//         <Projects />
+//         <Conatcts />
+//       </div>
+//       <img
+//         src="/Achernar.svg"
+//         className=" z-[-1] absolute right-[-20%]  top-[-60px]   "
+//       />
+//       <Particles/>
+//       <Footer />
+//     </main>
+//   );
+// }
 
-export default App;
+// export default App;
